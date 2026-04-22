0d5e68d8f6c40d6e98d256203e2f43818d4fd935
Shristi Koju <shristi.koju@yipl.com.np>
2026-04-22 10:48:29 +0545

    refactor: form submission code separate html and js

--- form.js
+++ form.js
@@ -1,25 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    if (req.url === '/') {
-        res.write(`
-        <form action="/submit" method="post">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name"><br><br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email"><br><br>
-            <button type="submit">Submit</button>
-        </form>
-        `);
-    } else if (req.url === '/submit') {
-        res.write(`
-                        <h1>Form submitted successfully!</h1>
-                        <button onclick="window.location.href='/'">Go Back</button>
-`)
-            ;
+    fs.readFile('html/form.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('<h1>404 Not Found</h1>');
+        }
 
-    }
-    res.end();
-
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+        } else if (req.url === '/submit') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(`<h1>Form Submitted Successfully</h1>
+                <p> Thank you for submitting the form.</p> 
+                    <button onclick = "window.location.href=\'/\'" > Go Back</button> `);
+        }
+        res.end();
+    })
 }).listen(3000)
